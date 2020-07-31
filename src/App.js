652e4569518f51779803efa821ad4d129a8651b1
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-31 15:04:59 -0500

    Finish main layout

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ function App() {
     <div>
       <div className="hero">
         <div className="container">
-          <nav className="main__navbar">
-            <h1 className="main__title">Rule of Thumb</h1>
-            <ul className="main__menu">
-              <li className="menu__item">Past Trials</li>
-              <li className="menu__item">How It Works</li>
-              <li className="menu__item">Log In / Sign Up</li>
-              <li className="menu__item">
-                <i className="fas fa-search"></i>
-              </li>
-            </ul>
-          </nav>
+          <header>
+            <nav className="main__navbar">
+              <h1 className="main__title">Rule of Thumb</h1>
+              <ul className="main__menu">
+                <li className="menu__item">Past Trials</li>
+                <li className="menu__item">How It Works</li>
+                <li className="menu__item">Log In / Sign Up</li>
+                <li className="menu__item">
+                  <i className="fas fa-search"></i>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <div className="hero__card">
             <div className="hero__card-content">
               <small>What's your opinion on</small>
@@ -70,125 +72,169 @@ function App() {
         <article className="previous__rulings">
           <h2>Previous Rulings</h2>
           <div className="cards__container">
-            <div className="card__rouling">
+            <div className="card__rouling kanye">
               <div className="card__content">
                 <div className="card__title">
-                  <div className="card__hand-thumbs">
+                  <div className="card__hand-thumbs up">
                     <i className="fas fa-thumbs-up"></i>
                   </div>
                   <div className="card__name">
                     <h4>Kanye West</h4>
                   </div>
                 </div>
+                <div className="card__info">
+                  <span>
+                    <strong>1 month ago</strong> in Entertainment
+                  </span>
+                </div>
                 <div className="card__description">
                   <p>
                     Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit
                     libero.
                   </p>
-                  <div className="card__more-info">
-                    <button>View Full Report</button>
-                    <span>1 month ago in Enternainment</span>
-                  </div>
                 </div>
-                <div className="card__votes">
+
+                <div className="card__vote">
+                  <div className="votes">
+                    <div className="votes__positive">
+                      <i className="fas fa-thumbs-up"></i>
+                    </div>
+                    <div className="votes__negative">
+                      <i className="fas fa-thumbs-down"></i>
+                    </div>
+                  </div>
+                  <button>Vote Now</button>
+                </div>
+
+                <div className="card__votes-results">
                   <div className="votes__up">
                     <i className="fas fa-thumbs-up"></i>
+                    <span>64%</span>
                   </div>
                   <div className="votes__down">
                     <i className="fas fa-thumbs-down"></i>
+                    <span>36%</span>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="card__rouling">
+            <div className="card__rouling mark">
               <div className="card__content">
                 <div className="card__title">
-                  <div className="card__hand-thumbs">
-                    <i className="fas fa-thumbs-up"></i>
+                  <div className="card__hand-thumbs down">
+                    <i className="fas fa-thumbs-down"></i>
                   </div>
                   <div className="card__name">
-                    <h4>Kanye West</h4>
+                    <h4>Mark Zuckenberg</h4>
                   </div>
+                </div>
+                <div className="card__info">
+                  <span>
+                    <strong>1 month ago</strong> in Business
+                  </span>
                 </div>
                 <div className="card__description">
-                  <p>
-                    Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit
-                    libero.
-                  </p>
-                  <div className="card__more-info">
-                    <button>View Full Report</button>
-                    <span>1 month ago in Enternainment</span>
-                  </div>
+                  <p>Thank you for voting!</p>
                 </div>
-                <div className="card__votes">
+
+                <div className="card__vote">
+                  <button>Vote Again</button>
+                </div>
+
+                <div className="card__votes-results">
                   <div className="votes__up">
                     <i className="fas fa-thumbs-up"></i>
+                    <span>36%</span>
                   </div>
                   <div className="votes__down">
                     <i className="fas fa-thumbs-down"></i>
+                    <span>64%</span>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="card__rouling">
+            <div className="card__rouling cristina">
               <div className="card__content">
                 <div className="card__title">
-                  <div className="card__hand-thumbs">
-                    <i className="fas fa-thumbs-up"></i>
+                  <div className="card__hand-thumbs down">
+                    <i className="fas fa-thumbs-down"></i>
                   </div>
                   <div className="card__name">
-                    <h4>Kanye West</h4>
+                    <h4>Cristina Fernández de Kirchner</h4>
                   </div>
+                </div>
+                <div className="card__info">
+                  <span>
+                    <strong>1 month ago</strong> in Politics
+                  </span>
                 </div>
                 <div className="card__description">
-                  <p>
-                    Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit
-                    libero.
-                  </p>
-                  <div className="card__more-info">
-                    <button>View Full Report</button>
-                    <span>1 month ago in Enternainment</span>
-                  </div>
+                  <p>Thank you for voting!</p>
                 </div>
-                <div className="card__votes">
+
+                <div className="card__vote">
+                  <div className="votes">
+                    <div className="votes__positive">
+                      <i className="fas fa-thumbs-up"></i>
+                    </div>
+                    <div className="votes__negative">
+                      <i className="fas fa-thumbs-down"></i>
+                    </div>
+                  </div>
+                  <button>Vote Now</button>
+                </div>
+
+                <div className="card__votes-results">
                   <div className="votes__up">
                     <i className="fas fa-thumbs-up"></i>
+                    <span>36%</span>
                   </div>
                   <div className="votes__down">
                     <i className="fas fa-thumbs-down"></i>
+                    <span>64%</span>
                   </div>
                 </div>
               </div>
             </div>
-
-            <div className="card__rouling">
+            <div className="card__rouling malala">
               <div className="card__content">
                 <div className="card__title">
-                  <div className="card__hand-thumbs">
+                  <div className="card__hand-thumbs up">
                     <i className="fas fa-thumbs-up"></i>
                   </div>
                   <div className="card__name">
-                    <h4>Kanye West</h4>
+                    <h4>Malala Yousafzai</h4>
                   </div>
+                </div>
+                <div className="card__info">
+                  <span>
+                    <strong>1 month ago</strong> in Entertainment
+                  </span>
                 </div>
                 <div className="card__description">
-                  <p>
-                    Vestibulum diam ante, porttitor a odio eget, rhoncus neque. Aenean eu velit
-                    libero.
-                  </p>
-                  <div className="card__more-info">
-                    <button>View Full Report</button>
-                    <span>1 month ago in Enternainment</span>
-                  </div>
+                  <p>Thank you for voting!</p>
                 </div>
-                <div className="card__votes">
+
+                <div className="card__vote">
+                  <div className="votes">
+                    <div className="votes__positive">
+                      <i className="fas fa-thumbs-up"></i>
+                    </div>
+                    <div className="votes__negative">
+                      <i className="fas fa-thumbs-down"></i>
+                    </div>
+                  </div>
+                  <button>Vote Now</button>
+                </div>
+
+                <div className="card__votes-results">
                   <div className="votes__up">
                     <i className="fas fa-thumbs-up"></i>
+                    <span>64%</span>
                   </div>
                   <div className="votes__down">
                     <i className="fas fa-thumbs-down"></i>
+                    <span>36%</span>
                   </div>
                 </div>
               </div>
@@ -197,11 +243,13 @@ function App() {
         </article>
 
         <article className="submit__name">
-          <div className="submit__info">
-            <p>Is there anyone else you would want us to add?</p>
-          </div>
-          <div className="submit__button">
-            <button>Submit a Name</button>
+          <div className="submit__container">
+            <div className="submit__info">
+              <p>Is there anyone else you would want us to add?</p>
+            </div>
+            <div className="submit__button">
+              <button>Submit a Name</button>
+            </div>
           </div>
         </article>
       </main>
