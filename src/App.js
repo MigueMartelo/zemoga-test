01434ddf05efb285c0bbfc8edc17e21830fda54f
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-31 15:23:32 -0500

    Finish footer layout

--- src/App.js
+++ src/App.js
@@ -254,18 +254,20 @@ function App() {
         </article>
       </main>
 
-      <footer>
-        <div className="footer__nav">
-          <ul>
-            <li>Terms and Conditions</li>
-            <li>Privacy Policy</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div className="footer__socials">
-          Fallow Us
-          <i className="fab fa-facebook-square"></i>
-          <i className="fab fa-twitter"></i>
+      <footer className="container">
+        <div className="footer__container">
+          <div className="footer__nav">
+            <ul>
+              <li>Terms and Conditions</li>
+              <li>Privacy Policy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div className="footer__socials">
+            <p>Fallow Us</p>
+            <i className="fab fa-facebook-square"></i>
+            <i className="fab fa-twitter"></i>
+          </div>
         </div>
       </footer>
     </div>
