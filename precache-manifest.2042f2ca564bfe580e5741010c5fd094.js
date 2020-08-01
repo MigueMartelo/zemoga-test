4033dc3758dd4ab354648926acd8d0669d1a53fe
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-08-01 13:18:35 -0500

    Updates

---
+++ precache-manifest.2042f2ca564bfe580e5741010c5fd094.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2892d5b5f9e1d82d10976c33b84e5b4",
+    "url": "/zemoga-test/index.html"
+  },
+  {
+    "revision": "4b4f18ff435f269bd9d7",
+    "url": "/zemoga-test/static/css/main.9506fcee.chunk.css"
+  },
+  {
+    "revision": "1a61fed2f5085195d691",
+    "url": "/zemoga-test/static/js/2.183048c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zemoga-test/static/js/2.183048c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4f18ff435f269bd9d7",
+    "url": "/zemoga-test/static/js/main.905628b8.chunk.js"
+  },
+  {
+    "revision": "744600200223ce38449c",
+    "url": "/zemoga-test/static/js/runtime-main.d3234ea6.js"
+  },
+  {
+    "revision": "36ebe8a93d0e861c77253e88ff3d2d53",
+    "url": "/zemoga-test/static/media/city.36ebe8a9.png"
+  },
+  {
+    "revision": "91c1da3bbc788063eac00a019b8e1074",
+    "url": "/zemoga-test/static/media/cristina.91c1da3b.png"
+  },
+  {
+    "revision": "3be34c7bb6e7642d425f9fb8603b0d0d",
+    "url": "/zemoga-test/static/media/francisco.3be34c7b.png"
+  },
+  {
+    "revision": "40ffef5d82dd94bedaa19f4ab90e4a2e",
+    "url": "/zemoga-test/static/media/kanye.40ffef5d.png"
+  },
+  {
+    "revision": "4e5eee0b67e9518587ebd1b82ccb4b0e",
+    "url": "/zemoga-test/static/media/malala.4e5eee0b.png"
+  },
+  {
+    "revision": "2cf40f7ac65550347e94e93052b9f659",
+    "url": "/zemoga-test/static/media/mark.2cf40f7a.png"
+  }
+]);
