b54ff4f8c2b347b41a70908885db30404790fbfd
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-08-01 01:37:09 -0500

    Add functionality to votes

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Card.scss';
 
 const Card = ({ person }) => {
   const { picture, name, category, description, thumbsUp, thumbsDown } = person;
 
-  const upDown = thumbsUp > thumbsDown ? 'up' : 'down';
+  const [voteUp, setVoteUp] = useState(thumbsUp);
+  const [voteDown, setVoteDown] = useState(thumbsDown);
+  const [percUp, setPercUp] = useState((voteUp * 100) / (voteUp + voteDown));
+  const [percDown, setPercDown] = useState((voteDown * 100) / (voteUp + voteDown));
+  const [vote, setVote] = useState('');
+  const [widthUp, setWidthUp] = useState({ width: percUp.toFixed() + '%' });
+  const [widthDown, setWidthDown] = useState({ width: percDown.toFixed() + '%' });
 
-  const percUp = (thumbsUp * 100) / (thumbsUp + thumbsDown);
-  const percDown = (thumbsDown * 100) / (thumbsUp + thumbsDown);
+  const upDown = voteUp > voteDown ? 'up' : 'down';
+
+  const handleVotes = (vote) => {
+    if (vote === 'up') {
+      setVoteUp(voteUp + 1);
+      setPercUp((voteUp * 100) / (voteUp + voteDown));
+      setWidthUp({ width: percUp.toFixed() + '%' });
+      setVote('');
+    } else {
+      setVoteDown(voteDown + 1);
+      setPercDown((voteDown * 100) / (voteUp + voteDown));
+      setWidthDown({ width: percDown.toFixed() + '%' });
+      setVote('');
+    }
+  };
+
+  const disableButtom = vote ? false : true;
 
   return (
     <div className={`card__rouling ${picture}`}>
@@ -32,22 +53,24 @@ const Card = ({ person }) => {
 
         <div className="card__vote">
           <div className="votes">
-            <div className="votes__positive">
+            <div className="votes__positive" onClick={() => setVote('up')}>
               <i className="fas fa-thumbs-up"></i>
             </div>
-            <div className="votes__negative">
+            <div className="votes__negative" onClick={() => setVote('down')}>
               <i className="fas fa-thumbs-down"></i>
             </div>
           </div>
-          <button>Vote Now</button>
+          <button onClick={() => handleVotes(vote)} disabled={disableButtom}>
+            Vote Now
+          </button>
         </div>
 
         <div className="card__votes-results">
-          <div className="votes__up">
+          <div className="votes__up" style={widthUp}>
             <i className="fas fa-thumbs-up"></i>
             <span>{percUp.toFixed()}%</span>
           </div>
-          <div className="votes__down">
+          <div className="votes__down" style={widthDown}>
             <i className="fas fa-thumbs-down"></i>
             <span>{percDown.toFixed()}%</span>
           </div>
