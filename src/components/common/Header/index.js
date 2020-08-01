3b324ae0bba0475ae09af06d2ed0c278846b368b
Miguel Angel Martelo Quiroz <miguemartelo1031@gmail.com>
2020-07-31 23:44:55 -0500

    Add router and make page to add navigation to the project

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import "./Header.scss";
+import './Header.scss';
 
 const Header = () => (
   <header>
     <nav className="main__navbar">
-      <h1 className="main__title">Rule of Thumb</h1>
+      <h1 className="main__title">
+        <Link to="/">Rule of Thumb</Link>
+      </h1>
       <ul className="main__menu">
-        <li className="menu__item">Past Trials</li>
-        <li className="menu__item">How It Works</li>
-        <li className="menu__item">Log In / Sign Up</li>
+        <li className="menu__item">
+          <Link to="/past-trials">Past Trials</Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/how-it-works">How It Works</Link>
+        </li>
+        <li className="menu__item">
+          <Link to="/">Log In / Sign Up</Link>
+        </li>
         <li className="menu__item">
           <i className="fas fa-search"></i>
         </li>
---
+++ src/pages/HowItWorks/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Layout from '../../components/common/Layout';
+
+const HowItWorks = () => {
+  const styles = {
+    paddingTop: 150,
+  };
+  return (
+    <Layout>
+      <main style={styles}>
+        <h2>How It Works</h2>
+      </main>
+    </Layout>
+  );
+};
+
+export default HowItWorks;
